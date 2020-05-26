3ac72c4757aca4a8b148e0398d5582eb3a601f4a
Mirko Galimberti <me@mirkogalimberti.com>
2020-05-26 15:43:02 +0200

    Make sure that we use appname + add base url

--- py4web_weasy/template.py
+++ py4web_weasy/template.py
@@ -1,24 +1,20 @@
 import weasyprint
-import yatl
-
 from py4web.core import Fixture, Template, response
-
 from py4web_weasy.utils import py4web_url_fetcher
 
 
 class WeasyTemplate(Fixture):
-
     def __init__(self, filename, path=None, delimiters="[[ ]]"):
         self.filename = filename
         self.path = path
         self.delimiters = delimiters
 
     def transform(self, output, shared_data=None):
-        output = Template(self.filename, self.path,
-                          self.delimiters).transform(output, shared_data)
+        output = Template(self.filename, self.path, self.delimiters).transform(
+            output, shared_data
+        )
         html = weasyprint.HTML(
-            string=output,
-            url_fetcher=py4web_url_fetcher,
+            string=output, base_url="", url_fetcher=py4web_url_fetcher,
         )
         rendered_doc = html.render()
         response.headers["Content-Type"] = "application/pdf"
--- py4web_weasy/utils.py
+++ py4web_weasy/utils.py
@@ -6,17 +6,22 @@ from py4web.core import request
 
 
 def py4web_url_fetcher(url, timeout=10, ssl_context=None):
-    if url.startswith(f'/{request.app_name}'):
+    if url.startswith("file://"):
+        url = url.split("file://")[-1]
         mime_type, encoding = mimetypes.guess_type(url)
         data = {
-            'mime_type': mime_type,
-            'encoding': encoding,
-            'filename': os.path.basename(url),
+            "mime_type": mime_type,
+            "encoding": encoding,
+            "filename": os.path.basename(url),
         }
+        url_parts = url.split("/")
+        if url_parts[0] == request.app_name:
+            url_parts = url_parts[1:]
 
         filepath = os.path.join(
-            os.environ["PY4WEB_APPS_FOLDER"], *url.split('/'))
-        data['file_obj'] = open(filepath, 'rb')
+            os.environ["PY4WEB_APPS_FOLDER"], request.app_name, *url_parts
+        )
+        data["file_obj"] = open(filepath, "rb")
         return data
 
     return weasyprint.default_url_fetcher(url, timeout, ssl_context)
